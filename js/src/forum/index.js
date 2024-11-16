38377d671cf67eb6e574e3ed7c0c7bca845b3f8f
xypp <2952795729@qq.com>
2024-11-16 23:29:49 +0800

    修复兼容性

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -6,46 +6,39 @@ import HeaderPrimary from 'flarum/common/components/HeaderPrimary';
 import LogInModal from 'flarum/common/components/LogInModal';
 import SignUpModal from 'flarum/common/components/SignUpModal';
 
-app.initializers.add('jslirola-login2seeplus', function()
-{
+app.initializers.add('jslirola-login2seeplus', function () {
     let jslirolaLogin2seeplusReplaceImages;
     const jslirolaLogin2seeplusImgMin = 150;
 
-    function get_link(trans)
-    {
+    function get_link(trans) {
         var newlink = document.createElement('a');
         newlink.setAttribute('class', 'l2sp');
         newlink.innerHTML = app.translator.trans(trans);
         return newlink.outerHTML;
     }
 
-    extend(HeaderPrimary.prototype, 'oninit', function ()
-    {
+    extend(HeaderPrimary.prototype, 'oninit', function () {
         jslirolaLogin2seeplusReplaceImages = JSON.parse(app.forum.attribute('jslirola.login2seeplus.image') || 0);
     });
 
-    extend(CommentPost.prototype, 'content', function(list)
-    {
+    extend(CommentPost.prototype, 'content', function (list) {
         if (app.session.user || this.isEditing())
             return;
 
-        let oldContent = list.find(e => e.attrs.className == 'Post-body').children[0].children;
+        let oldContent = list.find(e => e.attrs.className == 'Post-body').children.find(e => e.tag == "<").children;
         let newContent = oldContent;
         let subbedContent = false;
 
         // replace images
-        if (jslirolaLogin2seeplusReplaceImages)
-        {
+        if (jslirolaLogin2seeplusReplaceImages) {
 
             let imgCounter = 0;
-            newContent = newContent.replace(/<img((.(?!class=))*)\/?>/g, function(html)
-            {
+            newContent = newContent.replace(/<img((.(?!class=))*)\/?>/g, function (html) {
                 let img = $(html)[0];
                 let src = img.src;
 
                 let loader = new Image();
-                loader.onload = function()
-                {
+                loader.onload = function () {
                     let imgWidth = loader.width;
                     let imgHeight = loader.height;
                     imgWidth = imgWidth > jslirolaLogin2seeplusImgMin ? imgWidth : jslirolaLogin2seeplusImgMin;
@@ -63,17 +56,17 @@ app.initializers.add('jslirola-login2seeplus', function()
 
         if (subbedContent)
             newContent += '<div class="jslirolaLogin2seeplusAlert">' + app.translator.trans('jslirola-login2seeplus.forum.post',
-            {
-                login: "<a class='jslirolaLogin2seeplusLogin'>" + app.translator.trans('core.forum.header.log_in_link') + "</a>",
-                register: "<a class='jslirolaLogin2seeplusRegister'>" + app.translator.trans('core.forum.header.sign_up_link') + "</a>"
-            }).join('') + '</div>';
+                {
+                    login: "<a class='jslirolaLogin2seeplusLogin'>" + app.translator.trans('core.forum.header.log_in_link') + "</a>",
+                    register: "<a class='jslirolaLogin2seeplusRegister'>" + app.translator.trans('core.forum.header.sign_up_link') + "</a>"
+                }).join('') + '</div>';
 
-            list.find(e => e.attrs.className == 'Post-body').children[0] = m.trust(newContent);
+        list.find(e => e.attrs.className == 'Post-body').children =
+            list.find(e => e.attrs.className == 'Post-body').children.map(e => e.tag == "<" ? m.trust(newContent) : e);
 
     });
 
-    extend(CommentPost.prototype, 'oncreate', function()
-    {
+    extend(CommentPost.prototype, 'oncreate', function () {
         $('.Post-body a.l2sp').off('click').on('click', () => app.modal.show(LogInModal));
         $('.jslirolaLogin2seeplusLogin').off('click').on('click', () => app.modal.show(LogInModal));
         $('.jslirolaLogin2seeplusRegister').off('click').on('click', () => app.modal.show(SignUpModal));
